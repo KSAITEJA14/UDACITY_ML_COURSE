e23db290e9568113dfa0abf296ae86c5fb818657
ST14 <71367346+KSAITEJA14@users.noreply.github.com>
2022-04-20 19:06:43 +0530

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -17,15 +17,12 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print(len(features_train[0]))
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train,labels_train)
+labels_pred = clf.predict(features_test)
+print("accuracy is",accuracy_score(labels_test,labels_pred))
